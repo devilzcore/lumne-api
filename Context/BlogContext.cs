40331617bbf222565e5170febde8116c4492b8e0
devilzcore <aldosilva1@live.com>
2023-03-02 16:15:10 -0300

    fix(context): change var dbset for public

--- Context/BlogContext.cs
+++ Context/BlogContext.cs
@@ -10,10 +10,10 @@ namespace dotnet_angular_blog.Context
     {
     }
 
-    DbSet<User> Users { get; set; }
-    DbSet<UserProfile> UserProfiles { get; set; }
-    DbSet<Post> Posts { get; set; }
-    DbSet<Category> Categories { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserProfile> UserProfiles { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Category> Categories { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
