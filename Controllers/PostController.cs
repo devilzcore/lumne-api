c1772022104a840d639d43d314cdc119e12e91d2
devilzcore <aldosilva1@live.com>
2023-03-05 13:45:52 -0300

    feat: add PostController

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,35 +1,140 @@
 using dotnet_angular_blog.Context;
 using dotnet_angular_blog.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnet_angular_blog.Controllers
 {
   [Route("api/[controller]")]
+  [ApiController]
   public class PostController : Controller
   {
     private readonly BlogContext _context;
+
     public PostController(BlogContext context)
     {
       _context = context;
     }
 
+    [HttpGet]
     public IEnumerable<Post> GetAllPosts()
     {
       return _context.Posts.ToList();
     }
 
     [HttpGet("{id}")]
-    public Post GetById(int id)
+    public ActionResult GetById(int id)
     {
-      return _context.Posts.FirstOrDefault(x => x.Id == id);
+      var post = _context.Posts
+      .Include(p => p.Categories)
+      .FirstOrDefault(x => x.Id == id);
+
+      return Ok(post);
+    }
+
+    [HttpGet("category/{category}")]
+    public ActionResult GetByCategory(string category)
+    {
+      var post = _context.Posts
+      .Include(p => p.Categories)
+      .Where(p => p.Categories.Any(c => c.Name == category))
+      .ToList();
+
+      return Ok(post);
     }
 
     [HttpPost]
-    public IActionResult Create([FromBody] Post content)
+    public async Task<IActionResult> CreatePost([FromBody] Post post)
     {
-      _context.Posts.Add(content);
-      _context.SaveChanges();
-      return StatusCode(201, content);
+      // Create an empty categories list.
+      var categories = new List<Category>();
+
+      // Loop through each category of the post.
+      foreach (var categoryDto in post.Categories)
+      {
+        // Check if the category already exists in the database context or not.
+        var category = await _context.Categories
+          .FirstOrDefaultAsync(c => c.Name == categoryDto.Name);
+
+        // If null, create a new Category and add it to the database context.
+        if (category == null)
+        {
+          category = new Category { Name = categoryDto.Name };
+          _context.Categories.Add(category);
+        }
+
+        // Add the new/updated category to our list.
+        categories.Add(category);
+      }
+
+      // Update the Post object's Categories property to contain the added categories.
+      post.Categories = categories;
+
+      // Add the post to the database context and save changes to the database.
+      _context.Posts.Add(post);
+      await _context.SaveChangesAsync();
+
+      // Return a HTTP CreatedAtAction response with the new Post object included, along with its Id.
+      return CreatedAtAction("GetById", new { id = post.Id }, post);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdatePost([FromBody] Post post, int id)
+    {
+      // var postDto = _context.Posts.Find(id);
+      var postDto = await _context.Posts
+        .Include(p => p.Categories)
+        .FirstOrDefaultAsync(p => p.Id == id);
+
+      if (postDto == null)
+      {
+        return NotFound();
+      }
+
+      var categories = new List<Category>();
+
+      foreach (var categoryDto in post.Categories)
+      {
+        var category = await _context.Categories
+          .FirstOrDefaultAsync(c => c.Name == categoryDto.Name);
+
+        if (category == null)
+        {
+          category = new Category { Name = categoryDto.Name };
+          _context.Categories.Add(category);
+        }
+
+        categories.Add(category);
+      }
+
+      postDto.Title = post.Title;
+      postDto.Summary = post.Summary;
+      postDto.Image = post.Image;
+      postDto.Content = post.Content;
+      postDto.ReadingTime = post.ReadingTime;
+      postDto.Categories = categories;
+      postDto.EnumPostPermission = post.EnumPostPermission;
+      postDto.Author = post.Author;
+
+      _context.Posts.Update(postDto);
+      await _context.SaveChangesAsync();
+
+      return Ok(postDto);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Post>> DeletePost(int id)
+    {
+      var post = _context.Posts.Find(id);
+      if (post == null)
+      {
+        return NotFound();
+      }
+
+      _context.Posts.Remove(post);
+      await _context.SaveChangesAsync();
+
+      return NoContent();
     }
   }
 }
