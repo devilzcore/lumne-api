c22f310d87195a0227c53c1011b79502dc70c5e4
devilzcore <aldosilva1@live.com>
2023-03-10 12:22:10 -0300

    style: add component blog-login

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { BlogCardComponent } from './blog-card/blog-card.component';
 import { BlogMenuComponent } from './blog-menu/blog-menu.component';
+import { BlogLoginComponent } from './blog-login/blog-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogListComponent,
     BlogCardComponent,
-    BlogMenuComponent
+    BlogMenuComponent,
+    BlogLoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ ClientApp/src/app/blog-login/blog-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-login',
+  templateUrl: './blog-login.component.html',
+  styleUrls: ['./blog-login.component.scss']
+})
+export class BlogLoginComponent {
+
+}
