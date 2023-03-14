7ce4576eb6fd179535d0cf614e79ed3c13db1c0f
devilzcore <aldosilva1@live.com>
2023-03-14 19:16:56 -0300

    feat: add call api with blog-posts

--- ClientApp/src/app/blog-post/blog-post.component.ts
+++ ClientApp/src/app/blog-post/blog-post.component.ts
@@ -1,6 +1,8 @@
+import { Category } from './../../models/category';
 import { DataService } from './../../services/data.service';
-import { Category } from 'src/models/category';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Post } from 'src/models/post';
 
 @Component({
   selector: 'app-blog-post',
@@ -11,7 +13,20 @@ export class BlogPostComponent implements OnInit {
   category = {} as Category
   categories: Category[] = []
 
-  constructor(private categoryService: DataService) { }
+  postForm!: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoryService: DataService,
+    private postService: DataService
+  ) {
+    this.postForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      image: [''],
+      summary: [''],
+      content: ['']
+    })
+   }
 
   ngOnInit() {
     this.getCategories()
@@ -24,4 +39,19 @@ export class BlogPostComponent implements OnInit {
         console.log(categories)
       })
   }
+
+  onSubmit() {
+    const post: Post = {
+      title: this.postForm.controls['title'].value,
+      content: this.postForm.controls['content'].value,
+      categories: []
+    }
+
+    this.postService.savePost(post)
+      .subscribe(post => {
+        console.log('Save..')
+        this.postForm.reset()
+    })
+  }
+
 }
