70419d8e8fd0d6ce50c2d6fbdf8e9091c450e509
devilzcore <aldosilva1@live.com>
2023-03-15 18:24:03 -0300

    chore: update link router with date and title post

--- ClientApp/src/app/blog-card/blog-card.component.ts
+++ ClientApp/src/app/blog-card/blog-card.component.ts
@@ -2,6 +2,8 @@ import { DataService } from './../../services/data.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { Post } from '../../models/post'
 import { ActivatedRoute, Router } from '@angular/router';
+import { slugify } from 'src/helpers/string.utils';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-blog-card',
@@ -12,11 +14,19 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class BlogCardComponent {
   @Input() post!: Post
 
-  postUrl : string | undefined
+  postUrl: string | undefined
 
   constructor() { }
 
+  formatDate(date: Date): string {
+    return format(date, 'yyyy-MM-dd');
+  }
+
   ngOnInit() {
-    this.postUrl = `/posts/${this.post.id}`
+    const slug = slugify(this.post.title!)
+    const dateStr = this.formatDate(new Date(this.post.postedAt!))
+    this.postUrl = `/posts/${dateStr}/${this.post.id}-${slug}`
   }
 }
+
+
