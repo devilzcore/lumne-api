86098f772f56dca2d9f1963299cb056d094c664e
devilzcore <aldosilva1@live.com>
2023-03-15 17:18:49 -0300

    feat: add componente blog-posts

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { BlogPostComponent } from './blog-post/blog-post.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BlogPostsComponent } from './blog-posts/blog-posts.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
     BlogLoginComponent,
     BlogHomeComponent,
     PageNotFoundComponent,
-    BlogPostComponent
+    BlogPostComponent,
+    BlogPostsComponent
   ],
   imports: [
     BrowserModule,
