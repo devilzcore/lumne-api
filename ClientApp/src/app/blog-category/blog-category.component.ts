4475b13014120d69601a71e771f9fd37ad1884e5
devilzcore <aldosilva1@live.com>
2023-03-16 15:10:50 -0300

    chore: add function for return all posts by categories

--- ClientApp/src/app/blog-category/blog-category.component.ts
+++ ClientApp/src/app/blog-category/blog-category.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { DataService } from 'src/services/data.service';
+import { Post } from 'src/models/post';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-blog-category',
   templateUrl: './blog-category.component.html',
   styleUrls: ['./blog-category.component.scss']
 })
-export class BlogCategoryComponent {
+export class BlogCategoryComponent implements OnInit {
+  post = {} as Post
+  posts: Post[] = []
 
+  constructor(
+    private postService: DataService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.getPosts()
+  }
+
+  getPosts() {
+    this.route.params.subscribe((params) => {
+      const category = params['category']
+      return this.postService.getPostByCategories(category)
+      .subscribe(posts => this.posts = posts as Post[])
+    })
+  }
 }
